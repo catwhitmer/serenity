f4af6049e9a7afb40b23b97067cf0721de579e7e
catwhitmer <catwhitmer@gmail.com>
2020-05-04 18:54:09 -0400

    added username and email validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :appointments 
     has_many :massages, through :appointments
+
+    validates :username, :email, presence: true 
+    validates :username, uniqueness: true
 end
