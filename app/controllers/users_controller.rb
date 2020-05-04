6a9e8e7b469f8fbcbd8837c1bbe6eab236a9d804
catwhitmer <catwhitmer@gmail.com>
2020-05-04 19:00:38 -0400

    added the user create method and the private user_params method to the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else 
+      render :new
+    end
+  end
+
+  private
+
+  def user_params 
+    params.require(:user).permit(:username, :email, :password)
   end
 end
