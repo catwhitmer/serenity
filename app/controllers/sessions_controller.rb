1e5621909879c7fd12c1ee9fbff3971c17a83be1
catwhitmer <catwhitmer@gmail.com>
2020-05-05 18:58:11 -0400

    fixed the users#show method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def new
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(username: params[:user][:username])
   end
 
   def create
