49bab11c806d24f0ee86ffba9ec52cd53b9bb7b7
catwhitmer <catwhitmer@gmail.com>
2020-05-05 09:26:02 -0400

    added the massages and the appointments routes to config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+ 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/welcome', to: 'sessions#welcome'
+
+  resources :users, only: [:new, :create]
+  resources :massages
+  resources :appontments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
