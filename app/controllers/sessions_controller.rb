8d2b152afaf8f2ae114c703a099fc406e4fd06ed
catwhitmer <catwhitmer@gmail.com>
2020-05-05 18:47:08 -0400

    added the sessions create and the sessions destroy methods to the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,23 @@ class SessionsController < ApplicationController
   end
 
   def new
+    @user = User.find_by(username: params[:username])
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      sessions[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:message] = "Sorry, that did not work. Please try again!"
+      redirect_to '/login'
+    end
+  end
+
+  def destroy 
+    session.clear
+    redirect_to root_path
   end
 
 end
