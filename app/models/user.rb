758ac90ef67a7cf9f7403a91580a0f07b5cbacc0
catwhitmer <catwhitmer@gmail.com>
2020-05-05 17:06:30 -0400

    added the get methods to the button_to

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :appointments 
-    has_many :massages, through :appointments
+    has_many :massages, through: :appointments
 
-    validates :username, :email, presence: true 
+    validates :username, presence: true 
     validates :username, uniqueness: true
 end
