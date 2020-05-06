d4b7074fed1c37037456a34a3a91234a945ca4cc
catwhitmer <catwhitmer@gmail.com>
2020-05-06 08:51:18 -0400

    added the index, new, create, and the private appointment_params to the appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,26 @@
 class AppointmentsController < ApplicationController
 
     def index
+        @appointments = Appointment.all
+    end
+    
+    def new 
+        @appointment = Appointment.new
+    end 
+
+    def create
+        @appointment = Appointment.new(appointment_params)
+        @appointment.save
+        redirect_to appointment_path(@appointment)
+    end 
+
+    def show
     end
 
+
+    private
+
+    def appointment_params
+      params.require(:appointment).permit(:date, :location)
+    end
 end
