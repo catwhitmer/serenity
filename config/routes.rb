8529b3ec7f1664ea8da458334f94ab1f94613b03
catwhitmer <catwhitmer@gmail.com>
2020-05-06 10:06:16 -0400

    updated the routes in the config/routes file

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,12 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/singup', to: 'users#new'
   
-  resources :users
-  resources :massages
   resources :appointments
+  resources :users 
+  resources :massages do 
+    resources :appointments
+  end
+
   
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
