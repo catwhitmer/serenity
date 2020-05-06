e1f716fd536749cb628f1aa5a15133c058248bca
catwhitmer <catwhitmer@gmail.com>
2020-05-06 16:12:36 -0400

    added the edit and update actions to the massage controller

--- app/controllers/massages_controller.rb
+++ app/controllers/massages_controller.rb
@@ -5,7 +5,7 @@ class MassagesController < ApplicationController
     end
 
     def show
-      @massage = Massage.find_by(id: params[:id])
+     set_massage
     end
 
     def new
@@ -14,13 +14,33 @@ class MassagesController < ApplicationController
 
     def create
         @massage = Massage.new(massage_params)
-        @massage.save
+        if @massage.save
+          redirect_to massage_path(@massage)
+        else
+          render :new
+      end
+    end
+
+    def edit 
+      set_massage
+    end
+
+    def update
+      set_massage
+      if @massage.update(massage_params)
         redirect_to massage_path(@massage)
+      else
+        render :edit
+      end
     end
 
     private
 
     def massage_params
         params.require(:massage).permit(:category, :length)
+    end
+
+    def set_massage
+      @massage = Massage.find_by(id: params[:id])
     end
 end
