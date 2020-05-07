11397398f41a4e706d8a2df90bc35ac76ae603b7
catwhitmer <catwhitmer@gmail.com>
2020-05-07 12:45:35 -0400

    added a user nested route connecting users and appoinments

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,12 @@ Rails.application.routes.draw do
   
   resources :appointments
   resources :users 
+  resources :users do
+    resources :appointments
+  end
   resources :massages do 
     resources :appointments
   end
-
   
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
