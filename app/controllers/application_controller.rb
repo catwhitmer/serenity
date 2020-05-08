ac3577a85a3e73c071688c593f6645ffc8ea00ce
catwhitmer <catwhitmer@gmail.com>
2020-05-08 08:30:41 -0400

    added the authorized? helper method to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
     helper_method :current_user
     helper_method :logged_in?
+    helper_method :authorized?
 
     def current_user    
         @current_user ||= User.find_by(id: session[:user_id])  
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
 
     def logged_in?
        !current_user.nil?  
+    end
+
+    def authorized?(auth)
+      logged_in? && auth.user == current_user
     end
 end
