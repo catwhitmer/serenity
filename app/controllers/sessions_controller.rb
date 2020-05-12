e00e0328683df1ce477f3af38c3b522e246c4339
catwhitmer <catwhitmer@gmail.com>
2020-05-12 15:34:11 -0400

    moved the omniauth methods into the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,9 +18,25 @@ class SessionsController < ApplicationController
     end
   end
 
+  def fbcreate
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.username = auth['info']['name']
+      u.password = auth['info']['password_digest']
+    end
+ 
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
   def destroy 
     session.clear
     redirect_to root_path
+  end
+
+  private 
+
+  def auth
+    request.env['omniauth.auth']
   end
 
 end
